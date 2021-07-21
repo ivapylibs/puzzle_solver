faa09d3d592ccb5806a99738f7fc533e3f367c02
yunzhil <yunzhi.lin@gatech.edu>
2021-07-21 00:47:29 -0400

    add test scripts

--- testing/puzzle02.py
+++ testing/puzzle02.py
@@ -1,12 +1,13 @@
-#=============================== puzzle01 ==============================
+#=============================== puzzle02 ==============================
 #
 # @brief    Code to create a puzzle detector and working on well-separated puzzles
-#           (numpy), output movement instruction.
+#           (online resource), output movement instruction.
 #
-#=============================== puzzle01 ==============================
+#
+#=============================== puzzle02 ==============================
 
 #
-# @file     puzzle01.py
+# @file     puzzle02.py
 #
 # @author   Yunzhi Lin,         yunzhi.lin@gatech.edu
 # @date     2021/07/20 [created]
@@ -16,7 +17,7 @@
 #!  Tab is set to 4 spaces with conversion to spaces.
 #
 # @quit
-#=============================== puzzle01 ==============================
+#=============================== puzzle02 ==============================
 
 
 #==[0] Prep environment.
@@ -36,7 +37,7 @@ import puzzle_solver.detector_puzzle as detector_puzzle
 
 #--[1.1] Create the detector instance.
 
-improc = improcessor.basic(
+improc = improcessor.basic(cv2.cvtColor, (cv2.COLOR_BGR2GRAY,),
                   operator.lt, (127,),
                   improcessor.basic.to_uint8,())
 
@@ -44,12 +45,13 @@ puzzleDet = detector_puzzle.detector_puzzle(improc)
 
 #==[2] Apply puzzle solver to simple image.
 
-#--[2.1] Read an image from numpy
+#--[2.1] Read an image from the online source
 
-src_img = np.ones((100,100)) *255
-# Create two boxes, 20*10
-src_img[5:25, 5:15] = 0
-src_img[70:80, 60:80] = 0
+fpath = os.path.realpath(__file__)
+cpath = fpath.rsplit('/', 1)[0]
+
+# src_img = cv2.imread(cpath + '/data/image_1.jpg')
+src_img = cv2.imread(cpath + '/data/image_2.png')
 
 #--[2.2] Apply to simple image and visualize the output
 
@@ -59,5 +61,6 @@ print('Should see one segmented puzzle piece and one segmented template, \n'
       'The movement instruction is also displayed.\n')
 
 puzzleDet.process(src_img)
+
 #
-#=============================== puzzle01 ==============================
+#=============================== puzzle02 ==============================
