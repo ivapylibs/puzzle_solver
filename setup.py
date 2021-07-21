6120a7bcc5a2a4c60f9dd00172653fb855340c75
yunzhil <yunzhi.lin@gatech.edu>
2021-07-21 17:15:44 -0400

    update README

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 setup(name='puzzle_solver',
       version='1.0',
-      description='Classes implementing detection based processing pipelines.', 
+      description='Classes implementing detection based puzzle solving pipelines.', 
       author='IVALab',
       packages=['puzzle_solver'],
       )
