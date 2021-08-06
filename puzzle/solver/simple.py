383cf432d889356201cc0736cb52d337cb3f8326
Patricio A. Vela <pvela@gatech.edu>
2021-08-06 12:23:45 -0400

    Adding empty class stub for puzzle.solver.simple.
    Not fully fleshed out yet.  Neds more thought. Or just blast it out then evaluate later.

---
+++ puzzle/solver/simple.py
@@ -0,0 +1,50 @@
+#========================== puzzle.solver.simple =========================
+#
+# @class    puzzle.solver.simple
+#
+# @brief    A basic puzzle solver that just puts puzzle pieces where they
+#           belong based on sequential ordering.
+#
+# @file     simple.py
+#
+# @author   Patricio A. Vela,       pvela@gatech.edu
+# @author   WHO WHO
+# @date     2021/08/06  [started]
+#
+#!NOTE:
+#!  Indent is set to 2 spaces.
+#!  Tab is set to 4 spaces with conversion to spaces.
+#
+#========================== puzzle.solver.simple =========================
+
+#===== Dependencies / Packages
+#
+WHAT
+
+#===== Class Helper Elements
+#
+
+
+#
+#========================== puzzle.solver.simple =========================
+#
+
+class simple:
+
+  #=============================== simple ==============================
+  #
+  #
+  def __init__(self, theSol, thePuzzle):
+    pass
+
+    self.desired = theSol
+    self.current = thePuzzle
+
+  #============================== takeTurn =============================
+  #
+  #
+  def takeTurn(self):
+    pass
+
+#
+#========================== puzzle.solver.simple =========================
