63c00a9df191f778f0564cdca1eefde85b0bad38
yunzhil <yunzhi.lin@gatech.edu>
2021-09-02 14:19:12 -0400

    add another test script for regular

---
+++ puzzle/simulator/testing/basic03_withSolver.py
@@ -0,0 +1,174 @@
+#!/usr/bin/python3
+#============================ basic03_withSolver ===========================
+#
+# @brief    Test script with command from the solver. (60p img)
+#
+#============================ basic03_withSolver ===========================
+
+#
+# @file     basic03_withSolver.py
+#
+# @author   Yunzhi Lin,             yunzhi.lin@gatech.edu
+# @date     2021/08/30  [created]
+#
+#============================ basic03_withSolver ===========================
+
+
+#==[0] Prep environment
+import matplotlib.pyplot as plt
+import numpy as np
+import os
+import cv2
+import imageio
+import glob
+
+from puzzle.manager import manager, managerParms
+from puzzle.solver.simple import simple
+
+from puzzle.utils.imageProcessing import cropImage
+
+import improcessor.basic as improcessor
+from puzzle.parser.fromSketch import fromSketch
+from puzzle.parser.fromLayer import fromLayer, paramPuzzle
+from puzzle.builder.gridded import gridded, paramGrid
+
+from puzzle.simulator.basic import basic
+from puzzle.piece.edge import edge
+from puzzle.piece.regular import regular
+
+fpath = os.path.realpath(__file__)
+cpath = fpath.rsplit('/', 1)[0]
+
+#==[1] Read the source image and template.
+#
+theImageSol = cv2.imread(cpath + '/../../testing/data/balloon.png')
+# theImageSol = cv2.imread(cpath + '/../../testing/data/church.jpg')
+
+theImageSol = cv2.cvtColor(theImageSol, cv2.COLOR_BGR2RGB)
+
+# theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_60p_AdSt408534841.png')
+theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_15p_123rf.png')
+theImageSol = cropImage(theImageSol, theMaskSol_src)
+
+
+#==[1.1] Create an improcesser to obtain the mask.
+#
+
+improc = improcessor.basic(cv2.cvtColor, (cv2.COLOR_BGR2GRAY,),
+                  cv2.GaussianBlur, ((3, 3), 0,),
+                  cv2.Canny, (30, 200,),
+                  improcessor.basic.thresh, ((10,255,cv2.THRESH_BINARY),))
+
+theDet = fromSketch(improc)
+theDet.process(theMaskSol_src.copy())
+theMaskSol = theDet.getState().x
+
+#==[1.2] Extract info from theImage & theMask to obtain a board instance
+#
+# theLayer = fromLayer(paramPuzzle(areaThreshold=5000,pieceConstructor=regular))
+theLayer = fromLayer(paramPuzzle(areaThreshold=5000))
+
+theLayer.process(theImageSol,theMaskSol)
+theBoardSol = theLayer.getState()
+
+#==[1.3] Display the solution board
+#
+f, axarr = plt.subplots(1,2)
+bSource = theBoardSol.toImage(ID_DISPLAY=True)
+axarr[0].imshow(bSource)
+axarr[0].title.set_text('Source solution board')
+
+#==[2] Create an Grid instance and explode it
+#
+
+print('Running through test cases. Will take a bit.')
+
+theGrid = gridded.buildFrom_ImageAndMask(theImageSol, theMaskSol, theParams=paramGrid(areaThreshold=1000))
+
+epImage, epBoard = theGrid.explodedPuzzle(dx=200,dy=200)
+
+#==[2.1] Create a new Grid instance from the images
+#
+
+# @note
+# Not a fair game to directly use the epBoard
+# Instead, should restart from images
+
+improc = improcessor.basic(cv2.cvtColor, (cv2.COLOR_BGR2GRAY,),
+                  improcessor.basic.thresh, ((5,255,cv2.THRESH_BINARY),),
+                  cv2.GaussianBlur, ((3, 3), 0,),
+                  cv2.Canny, (30, 200,),
+                  improcessor.basic.thresh, ((10,255,cv2.THRESH_BINARY),))
+
+theDet = fromSketch(improc)
+theDet.process(epImage.copy())
+theMaskSol_new = theDet.getState().x
+
+theGrid_new = gridded.buildFrom_ImageAndMask(epImage, theMaskSol_new, theParams=paramGrid(areaThreshold=5000,pieceConstructor=regular))
+
+axarr[1].imshow(epImage)
+axarr[1].title.set_text('Exploded view')
+
+plt.show()
+#==[3] Create match by manager
+#
+theManager = manager(theBoardSol, managerParms(matcher=edge()))
+# theManager.process(epBoard)
+theManager.process(theGrid_new.solution)
+
+#==[4] Create simple sovler and set up the match
+#
+theSolver = simple(theBoardSol, theGrid_new.solution)
+
+theSolver.setMatch(theManager.pAssignments)
+
+#==[5] Create a simulator for display
+#
+theSim = basic(theGrid_new.solution)
+
+#==[6] Start the solver to take turns, display the updated board.
+#
+
+plt.ion()
+
+# saveMe = True
+saveMe = False
+
+if saveMe:
+  f.savefig(cpath + f'/data/theBoardExplode.png')
+
+# num of size() actions at most
+for i in range(1+theSolver.desired.size()):
+
+  # Since we use the same instance in the simulator and the solver,
+  # it will update automatically
+  theSim.display(ID_DISPLAY=True)
+
+  theSim.fig.suptitle(f'Step {i}', fontsize=20)
+  plt.pause(1)
+
+  if i==0:
+    # Display the original one at the very beginning
+    print(f'The original measured board')
+
+  if saveMe:
+    theSim.fig.savefig(cpath + f'/data/explode02_simple_step{str(i).zfill(2)}.png')
+
+  if i < theSolver.desired.size():
+    print(f'Step {i+1}:')
+    theSolver.takeTurn(defaultPlan='order')
+
+plt.ioff()
+# plt.draw()
+
+if saveMe:
+  # Build GIF
+  with imageio.get_writer(cpath + f'/data/demo_simple_explode02.gif', mode='I', fps=1) as writer:
+      filename_list = glob.glob(cpath + f'/data/explode02_simple_step*.png')
+      filename_list.sort()
+      for filename in filename_list:
+          image = imageio.imread(filename)
+          writer.append_data(image)
+
+#
+#============================ basic03_withSolver ===========================
