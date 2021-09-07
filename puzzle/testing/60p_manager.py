ee187bf01417b9934f352abee012d692ac5112a9
yunzhi <yunzhi.lin@gatech.edu>
2021-09-07 10:28:40 -0400

    update process

--- puzzle/testing/60p_manager.py
+++ puzzle/testing/60p_manager.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
-#============================= 15p_manager =============================
+#============================= 60p_manager =============================
 #
 # @brief    Tests the core functionality of the puzzle.manager class. The manager
 #           will have access to measurement and solution images & masks to generate
-#           associations between them. (15p img)
+#           associations between them. (60p img)
 #
 #
-#============================= 15p_manager =============================
+#============================= 60p_manager =============================
 
 #
-# @file     15p_manager.py
+# @file     60p_manager.py
 #
 # @author   Yunzhi Lin,             yunzhi.lin@gatech.edu
-# @date     2021/08/28 [created]
+# @date     2021/09/07 [created]
 #
-#============================= 15p_manager =============================
+#============================= 60p_manager =============================
 
 #==[0] Prep environment
 import matplotlib.pyplot as plt
@@ -23,6 +23,9 @@ import os
 import cv2
 
 from puzzle.manager import manager, managerParms
+
+from puzzle.utils.imageProcessing import cropImage
+
 
 import improcessor.basic as improcessor
 from puzzle.parser.fromSketch import fromSketch
@@ -38,11 +41,14 @@ cpath = fpath.rsplit('/', 1)[0]
 
 #==[1] Read the source image and template.
 #
-theImageSol = cv2.imread(cpath + '/data/balloon.png')
+# theImageSol = cv2.imread(cpath + '/data/balloon.png')
+theImageSol = cv2.imread(cpath + '/data/cocacola.jpg')
+# theImageSol = cv2.imread(cpath + '/data/church.jpg')
+
 theImageSol = cv2.cvtColor(theImageSol, cv2.COLOR_BGR2RGB)
 
-theMaskSol_src = cv2.imread(cpath + '/data/puzzle_15p_123rf.png')
-
+theMaskSol_src = cv2.imread(cpath + '/data/puzzle_60p_AdSt408534841.png')
+theImageSol = cropImage(theImageSol, theMaskSol_src)
 
 #==[1.1] Create an improcesser to obtain the mask.
 #
@@ -58,7 +64,8 @@ theMaskSol = theDet.getState().x
 
 #==[1.2] Extract info from theImage & theMask to obtain a board instance
 #
-theLayer = fromLayer(paramPuzzle(areaThreshold=5000, pieceConstructor=regular))
+theLayer = fromLayer(paramPuzzle(areaThreshold=5000,pieceConstructor=regular))
+
 theLayer.process(theImageSol,theMaskSol)
 theBoardSol = theLayer.getState()
 
@@ -117,6 +124,13 @@ axarr[1].title.set_text('Solution')
 print('The first index refers to the measured board while the second one refers to the solution board.')
 print(theManager.pAssignments)
 
+num_failure =0
+for pair in theManager.pAssignments:
+  if pair[0]!=pair[1]:
+    num_failure=num_failure+1
+
+print('Failure cases:',num_failure)
+
 plt.show()
 #
-#============================= 15p_manager =============================
+#============================= 60p_manager =============================
