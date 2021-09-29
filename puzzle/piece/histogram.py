bcd3dbbc896bc891817d113b03341b9b1041da20
yunzhil <yunzhi.lin@gatech.edu>
2021-09-29 17:51:36 -0400

    update histogram feature for color

---
+++ puzzle/piece/histogram.py
@@ -0,0 +1,94 @@
+# ================================ puzzle.piece.histogram ================================
+#
+# @brief    Use histogram for color to establish similarity.
+#
+# ================================ puzzle.piece.histogram ================================
+#
+# @file     histogram.py
+#
+# @author   Yunzhi Lin,             yunzhi.lin@gatech.edu
+# @date     2021/09/29 [created]
+#
+#
+# ================================ puzzle.piece.histogram ================================
+
+import math
+
+# ===== Environment / Dependencies
+#
+import cv2
+import numpy as np
+
+from puzzle.piece.matchDifferent import matchDifferent
+from puzzle.piece.template import template
+
+
+#
+# ================================ puzzle.piece.histogram ================================
+#
+class histogram(matchDifferent):
+
+    def __init__(self, tau=0.3):
+        """
+        @brief  Constructor for the puzzle piece histogram class.
+
+        Args:
+            tau: The threshold param to determine difference.
+        """
+
+        super(histogram, self).__init__(tau)
+
+    def process(self, piece):
+        """
+        @brief  Compute histogram from the raw puzzle data.
+                See https://opencv-tutorial.readthedocs.io/en/latest/histogram/histogram.html
+
+        Args:
+            piece: A template instance saving a piece's info.
+
+        Returns:
+            The histogram.
+        """
+
+        if issubclass(type(piece), template):
+            if piece.y.colorFea:
+                return piece.y.colorFea
+        else:
+            raise ('The input type is wrong. Need a template instance or a puzzleTemplate instance.')
+
+        cv2.imshow('demo',piece.y.mask)
+        cv2.waitKey()
+
+        # Convert to HSV space for comparison, see https://theailearner.com/tag/cv2-comparehist/
+        img_hsv = cv2.cvtColor(piece.y.image, cv2.COLOR_RGB2HSV)
+        hist = cv2.calcHist([img_hsv], [0, 1], piece.y.mask, [180, 256], [0, 180, 0, 256])
+        cv2.normalize(hist, hist, alpha=0, beta=1, norm_type=cv2.NORM_MINMAX)
+
+        # hist = cv2.calcHist([piece.y.image], [0, 1, 2], piece.y.mask, [32, 32, 32],
+        #                     [0, 256, 0, 256, 0, 256])
+
+        piece.y.colorFea = hist
+        return hist
+
+    def score(self, piece_A, piece_B):
+        """
+        @brief  Compute the score between two passed puzzle piece data.
+
+        Args:
+            piece_A: A template instance saving a piece's info.
+            piece_B: A template instance saving a piece's info.
+
+        Returns:
+            Distance.
+        """
+
+        hist_A = self.process(piece_A)
+        hist_B = self.process(piece_B)
+
+        # Range from 0-1, smaller means closer
+        distance = cv2.compareHist(hist_A, hist_B, cv2.HISTCMP_BHATTACHARYYA)
+
+        return distance
+
+#
+# ================================ puzzle.piece.histogram ================================
---
+++ puzzle/piece/testing/basic07_histogram.py
@@ -0,0 +1,120 @@
+#!/usr/bin/python3
+# ============================ basic06_histogram ===========================
+#
+# @brief    Test script for the most basic functionality of histogram features
+#           for puzzle pieces. (60p img)
+#
+# ============================ basic06_histogram ===========================
+
+#
+# @file     basic06_histogram.py
+#
+# @author   Yunzhi Lin,             yunzhi.lin@gatech.edu
+# @date     2021/09/29  [created]
+#
+# ============================ basic06_histogram ===========================
+
+
+import os
+
+import cv2
+import improcessor.basic as improcessor
+# ==[0] Prep environment
+import matplotlib.pyplot as plt
+
+from puzzle.board import board
+from puzzle.builder.gridded import gridded, paramGrid
+from puzzle.parser.fromLayer import fromLayer, paramPuzzle
+from puzzle.parser.fromSketch import fromSketch
+from puzzle.piece.regular import regular
+from puzzle.piece.histogram import histogram
+from puzzle.utils.imageProcessing import cropImage
+
+fpath = os.path.realpath(__file__)
+cpath = fpath.rsplit('/', 1)[0]
+
+# ==[1] Read the source image and template.
+#
+theImageSol = cv2.imread(cpath + '/../../testing/data/balloon.png')
+theImageSol = cv2.cvtColor(theImageSol, cv2.COLOR_BGR2RGB)
+
+theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_60p_AdSt408534841.png')
+theImageSol = cropImage(theImageSol, theMaskSol_src)
+
+# ==[1.1] Create an improcesser to obtain the mask.
+#
+
+improc = improcessor.basic(cv2.cvtColor, (cv2.COLOR_BGR2GRAY,),
+                           cv2.GaussianBlur, ((3, 3), 0,),
+                           cv2.Canny, (30, 200,),
+                           improcessor.basic.thresh, ((10, 255, cv2.THRESH_BINARY),))
+
+theDet = fromSketch(improc)
+theDet.process(theMaskSol_src.copy())
+theMaskSol = theDet.getState().x
+
+# ==[1.2] Extract info from theImage & theMask to obtain a board instance
+#
+theLayer = fromLayer(paramPuzzle(areaThreshold=5000))
+
+theLayer.process(theImageSol, theMaskSol)
+theBoardSol = theLayer.getState()
+
+# ==[1.3] Create an Grid instance and explode it into two new boards
+#
+
+print('Running through test cases. Will take a bit.')
+
+theGrid = gridded.buildFrom_ImageAndMask(theImageSol, theMaskSol,
+                                         theParams=paramGrid(areaThreshold=5000, pieceConstructor=regular, reorder=True))
+
+epImage, epBoard = theGrid.explodedPuzzle(dx=100, dy=100)
+
+# ==[1.4] Create a new Grid instance from the images
+#
+
+# @note
+# Not a fair game to directly use the epBoard
+# Instead, should restart from images
+
+improc = improcessor.basic(cv2.cvtColor, (cv2.COLOR_BGR2GRAY,),
+                           improcessor.basic.thresh, ((5, 255, cv2.THRESH_BINARY),),
+                           cv2.GaussianBlur, ((3, 3), 0,),
+                           cv2.Canny, (30, 200,),
+                           improcessor.basic.thresh, ((10, 255, cv2.THRESH_BINARY),))
+
+theDet = fromSketch(improc)
+theDet.process(epImage.copy())
+theMaskSol_new = theDet.getState().x
+
+theGrid_new = gridded.buildFrom_ImageAndMask(epImage, theMaskSol_new,
+                                             theParams=paramGrid(areaThreshold=1000, pieceConstructor=regular, reorder=True))
+
+# ==[1.5] Focus on a single puzzle piece and duplicate it with a new location
+#
+
+theRegular_A = theGrid.solution.pieces[1]
+theRegular_B = theGrid_new.solution.pieces[1]
+
+# ==[2] Create a new board
+#
+theBoard = board()
+theBoard.addPiece(theRegular_A)
+theBoard.addPiece(theRegular_B)
+
+# ==[3] Create an edge matcher
+#
+
+theMatcher = histogram()
+
+# ==[4] Display the new board and the comparison result.
+#
+print('Should see True.')
+print(theMatcher.compare(theRegular_A, theRegular_B))
+
+theBoard.display()
+
+plt.show()
+
+#
+# ============================ basic06_histogram ===========================
