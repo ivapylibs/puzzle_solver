d8965497e38ef615de60e4d38e88a795b8bca0d0
yunzhil <yunzhi.lin@gatech.edu>
2021-11-25 01:38:52 -0500

    faster rendering with PyGame

--- puzzle/simulator/simTime.py
+++ puzzle/simulator/simTime.py
@@ -1,36 +1,36 @@
-# ========================= puzzle.simulator.simTime ========================
+# ========================= puzzle.simulator.simTimeless ========================
 #
-# @class    puzzle.simulator.SimTime
+# @class    puzzle.simulator.SimTimeless
 #
-# @brief    The simulator that also simulates the time effect.
-#           Each simulation step will have a fixed time length,
-#           and the speed of the agent movement and the length
-#           of the agent's pause will be simulated.
+# @brief    The simulator that simulate the puzzle solving process
+#           without any time effect.
+#           The agent will observe the board and attempt to solve it until finished
 #
-# ========================= puzzle.simulator.simTime ========================
+# ========================= puzzle.simulator.simTimeless ========================
 #
-# @file     simTime.py
+# @file     simTimeless.py
 #
-# @author   Yiye Chen,              yychen2019@gatech.edu
+# @author   Yunzhi Lin,              yunzhi.lin@gatech.edu
 #
-# @date     2021/09/10
+# @date     2021/11/25
 #
 #
-# ========================= puzzle.simulator.simTime ========================
-
+# ========================= puzzle.simulator.simTimeless ========================
 
 import math
+from copy import deepcopy
 from dataclasses import dataclass
 
+import cv2
+import matplotlib.pyplot as plt
 import numpy as np
+import pygame
 
-from puzzle.builder.board import Board
-from puzzle.simulator.agent import Agent
-from puzzle.simulator.simTimeless import SimTimeLess, ParamST
+from puzzle.simulator.simTimeless import SimTimeLess, ParamSTL
 
 
 @dataclass
-class ParamSTL(ParamST):
+class ParamST(ParamSTL):
     """
     @param  canvas_H            The height of the whole scene
     @param  canvas_W            The width of the whole scene
@@ -39,88 +39,56 @@ class ParamSTL(ParamST):
     @param  speed               Unit: pixel/s. The speed of the agent movement
     @param  static_duration     Unit: s(econd). The duration of the static actions
     """
-    delta_t: float = 0.01
-    speed: float = 200
-    static_duration: float = 0.5
+    delta_t: float = 0.1
+    speed: float = 100
+    static_duration: float = 0.1
 
 
 class SimTime(SimTimeLess):
     """
-    @brief: The time-aware simulator that simulate the time effects
+    @brief: The timeless simulator class that simulates the puzzle solving progress.
 
-    On top of the timeless simulator,this simulator simulates the time when
-    executing the agent action. To be more specific:
-    1. Each simulation step is assumed to have a fixed time length
-    2. The agent dynamic actions (e.g. Move) is assumed to have a fixed speed
-    3. The agent static actions (e.g. Pause, Pick, Place) are assumed to have a fixed
-        duration
+    The class stores an initial board, a solution board, and an agent instance.
+    During the simulation, the simulator will execute the following process:
+    1. Let the agent observe the board
+    2. Let the agent process the board, which means:
+        2.1 Plan actions if don't know what to do (no more stored actions)
+        2.2 Execute the next planned actions
+    3. If no more process result, meaning no more plans or actions to be executed,
+        then end the simulation. Otherwise repreat the above process
 
-    @param[in]  init_board          The initial board
-    @param[in]  sol_board           The solution board
+    @param[in]  thePuzzle           The current board
     @param[in]  agent               The puzzle solving agent
-    @param[in]  param               ParamST instance. It stores the parameters
-                                    related to the time effect
+    @param[in]  param               The parameters
     """
 
-    def __init__(self, init_board: Board, sol_board: Board, agent: Agent,
-                 param: ParamSTL = ParamSTL()):
-        super().__init__(init_board, sol_board, agent, param=param)
-        # store the parameters
-        self.param = param
+    def __init__(self, thePuzzle, agent, param=ParamST()):
 
-        # cached action, argument, and time
-        self.cache_action = None  # The next action to execute
-        self.cache_arg = None  # The next action argument
-        self.timer = -1  # The timer
+        super(SimTime, self).__init__(thePuzzle, agent, param=param)
 
-    def simulate_step(self):
-        """
-        Overwrite the simulate_step function
-        """
+        self.timer = self.param.static_duration  # The timer
 
-        SUCCESS = True  # < Whether the simulation is still successfully running
-        flag_finish = False  # < Whether the current cached action has been finished
+        # Setting up FPS
+        self.FPS = 60
+        self.FramePerSec = pygame.time.Clock()
 
-        if self.cache_action is None:
-            # if no more stored action, meaning the last action has been executed.
-            # Then will let the agent plan again (if has unfinished plans then will simply do nothign)
-            # and pop out the new action
-            SUCCESS, _, _ = self.agent.process(self.cur_board, execute=False)
-            if SUCCESS:
-                self.cache_action, self.cache_arg = self.agent.pop_action()
-
-                # If the next action to executed is static, then start the timer.
-                if self.cache_action != "move":
-                    self.timer = self.param.static_duration
-            else:
-                return False
-
-        elif self.cache_action == "move":
-            # The move action
-            flag_finish = self._move_step(self.cache_arg)
-        else:
-            # The static actions
-            flag_finish = self._pause_step()
-
-        # if the cached action is finished, reset the cache and the timer
-        if flag_finish:
-            self.reset_cache()
-
-        return SUCCESS
+        # To save the display window
+        self.DISPLAYSURF = None
 
     def _move_step(self, target_loc):
         """
-        Move a step given the target location
-        Note that the target location might not be reachable within a step's time length
+        @brief  Move a step given the target location.
+                Note that the target location might not be reachable within a step's time length.
+        Args:
+            target_loc: The target location of the "move"
 
-        @param[in]  target_loc          The target location of the "move"
-
-        @param[out] flag_finish        Binary. Indicator of whether the target location
-                                        has been reached
+        Returns:
+            flag_finish(Indicator of whether the target location has been reached)
         """
+
         # distance
-        delta_x = target_loc[0] - self.agent.loc[0]
-        delta_y = target_loc[1] - self.agent.loc[1]
+        delta_x = target_loc[0] - self.agent.app.rLoc[0]
+        delta_y = target_loc[1] - self.agent.app.rLoc[1]
         distance = math.sqrt(delta_x ** 2 + delta_y ** 2)
 
         # determine where to end up
@@ -128,31 +96,38 @@ class SimTime(SimTimeLess):
         if distance >= self.param.delta_t * self.param.speed:
             x_step = delta_x * (self.param.delta_t * self.param.speed) / distance
             y_step = delta_y * (self.param.delta_t * self.param.speed) / distance
-            step_loc[0] = self.agent.loc[0] + x_step
-            step_loc[1] = self.agent.loc[1] + y_step
+            step_loc[0] = self.agent.app.rLoc[0] + x_step
+            step_loc[1] = self.agent.app.rLoc[1] + y_step
             flag_finish = False
         else:
             step_loc[0] = target_loc[0]
             step_loc[1] = target_loc[1]
             flag_finish = True
 
-        # execute
-        self.agent.execute("move", step_loc)
+        # Todo: May update the bounds later. Could be slightly off.
+        if step_loc[0] < 0 or step_loc[1] < 0 \
+                or step_loc[0] + self.agent.app.size()[0] >= self.canvas.shape[1] \
+                or step_loc[1] + self.agent.app.size()[1] >= self.canvas.shape[0]:
+            print('Out of the bounds!')
+            flag_finish = True
+        else:
+            # Execute
+            self.agent.execute(self.puzzle, "move", step_loc)
 
         return flag_finish
 
     def _pause_step(self):
         assert self.cache_action != "move"
 
-        # if have not been executed, then execute first before pause there
-        if self.timer == self.param.static_duration:
-            self.agent.execute(self.cache_action, self.cache_arg, board=self.cur_board)
+        # If have not been executed, then execute first before pause there
+        if abs(self.timer - self.param.static_duration) < 1e-04:
+            self.agent.execute(self.puzzle, self.cache_action, self.cache_arg)
 
-            # continue to run the timer
+        # Continue to run the timer
         self.timer -= self.param.delta_t
 
-        # if timer is below zero, then it is up!
-        if self.timer <= 0:
+        # If timer is below zero, then it is up!
+        if self.timer < 0:
             flag_finish = True
         else:
             flag_finish = False
@@ -162,4 +137,112 @@ class SimTime(SimTimeLess):
     def reset_cache(self):
         self.cache_arg = None
         self.cache_action = None
-        self.timer = -1
+        self.timer = self.param.static_duration
+
+    def simulate_step(self):
+        """
+        Overwrite the simulate_step function
+        """
+
+        if self.cache_action is None:
+            return True
+        elif self.cache_action == "move":
+            # The move action
+            flag_finish = self._move_step(self.cache_arg)
+        else:
+            # The static actions
+            flag_finish = self._pause_step()
+
+        # if the cached action is finished, reset the cache and the timer
+        if flag_finish:  # < Whether the current cached action has been finished
+            self.reset_cache()
+
+        return flag_finish
+
+    def display(self, ID_DISPLAY=True, CONTOUR_DISPLAY=True):
+        """
+        @brief  Displays the current puzzle board.
+
+        Args:
+            ID_DISPLAY: Flag indicating ID_DISPLAY or not.
+            CONTOUR_DISPLAY: Flag indicating CONTOUR_DISPLAY or not.
+        """
+
+        def press(key):
+
+            if key == pygame.K_UP:
+                self.cache_action = "move"
+                self.cache_arg = np.array([0, -self.param.displacement]) + self.agent.app.rLoc
+            elif key == pygame.K_LEFT:
+                self.cache_action = "move"
+                self.cache_arg = np.array([-self.param.displacement, 0]) + self.agent.app.rLoc
+            elif key == pygame.K_DOWN:
+                self.cache_action = "move"
+                self.cache_arg = np.array([0, self.param.displacement]) + self.agent.app.rLoc
+            elif key == pygame.K_RIGHT:
+                self.cache_action = "move"
+                self.cache_arg = np.array([self.param.displacement, 0]) + self.agent.app.rLoc
+            elif key == pygame.K_z:
+                self.cache_action = "pick"
+                self.cache_arg = None
+            elif key == pygame.K_c:
+                self.cache_action = "place"
+                self.cache_arg = None
+            else:
+                return
+
+            cache_image = self.puzzle.toImage(np.zeros_like(self.canvas), ID_DISPLAY=ID_DISPLAY,
+                                              BOUNDING_BOX=False)
+
+            while 1:
+
+                finish_flag = self.simulate_step()
+
+                if finish_flag is True:
+                    cache_image = self.puzzle.toImage(np.zeros_like(self.canvas), ID_DISPLAY=ID_DISPLAY,
+                                                      BOUNDING_BOX=False)
+
+                theImage = deepcopy(cache_image)
+                self.agent.placeInImage(theImage, CONTOUR_DISPLAY=CONTOUR_DISPLAY)
+
+                theImage_demo = cv2.resize(theImage, (0, 0), fx=0.5, fy=0.5)
+                background = pygame.surfarray.make_surface(np.moveaxis(theImage_demo, 0, 1))
+
+                self.DISPLAYSURF.blit(background, (0, 0))
+
+                pygame.display.update()
+                self.FramePerSec.tick(self.FPS)
+
+                if finish_flag is True:
+                    break
+
+        if not self.fig:
+            self.fig = plt.figure()
+
+        theImage = self.puzzle.toImage(np.zeros_like(self.canvas), ID_DISPLAY=ID_DISPLAY, BOUNDING_BOX=False)
+        self.agent.placeInImage(theImage, CONTOUR_DISPLAY=CONTOUR_DISPLAY)
+
+        theImage_demo = cv2.resize(theImage, (0, 0), fx=0.5, fy=0.5)
+        background = pygame.surfarray.make_surface(np.moveaxis(theImage_demo, 0, 1))
+
+        # Initializing
+        pygame.init()
+
+        # Create a white screen
+        BLACK = (0, 0, 0)
+        self.DISPLAYSURF = pygame.display.set_mode((theImage_demo.shape[1], theImage_demo.shape[0]))
+        self.DISPLAYSURF.fill(BLACK)
+        pygame.display.set_caption("Puzzle Solver")
+        self.DISPLAYSURF.blit(background, (0, 0))
+
+        # Game Loop
+        while True:
+
+            pressed_keys = pygame.key.get_pressed()
+            events = pygame.event.get()
+            for event in events:
+                if event.type == pygame.KEYDOWN:
+                    press(event.key)
+
+            pygame.display.update()
+            self.FramePerSec.tick(self.FPS)
--- puzzle/simulator/simTimeless.py
+++ puzzle/simulator/simTimeless.py
@@ -10,34 +10,37 @@
 #
 # @file     simTimeless.py
 #
-# @author   Yiye Chen,              yychen2019@gatech.edu
+# @author   Yunzhi Lin,              yunzhi.lin@gatech.edu
 #
-# @date     2021/09/10
+# @date     2021/11/25
 #
 #
 # ========================= puzzle.simulator.simTimeless ========================
 
+import sys
 from copy import deepcopy
 from dataclasses import dataclass
 
 import matplotlib.pyplot as plt
 import numpy as np
 
-from puzzle.builder.board import Board
-from puzzle.simulator.agent import Agent
+from puzzle.simulator.basic import Basic
 
+
+# from puzzle.simulator.agent import Agent
 
 @dataclass
-class ParamST():
+class ParamSTL:
     """
     @param canvas_H             The height of the whole scene
     @param canvas_W             The width of the whole scene
     """
-    canvas_H: int = 200  # <- The height of the scene
-    canvas_W: int = 200  # <- The width of the scene
+    canvas_H: int = 2500  # <- The height of the scene
+    canvas_W: int = 3500  # <- The width of the scene
+    displacement: int = 100  # <- The unit movement of the agent
 
 
-class SimTimeLess():
+class SimTimeLess(Basic):
     """
     @brief: The timeless simulator class that simulates the puzzle solving progress.
 
@@ -56,84 +59,79 @@ class SimTimeLess():
     @param[in]  param               The parameters
     """
 
-    def __init__(self, init_board: Board, sol_board: Board, agent: Agent, param: ParamST = ParamST()):
-        self.init_board = init_board  # Initial board
-        self.sol_board = sol_board  # Solution board
-        self.cur_board = init_board  # The current board. At first it will be only the inital board
+    def __init__(self, thePuzzle, agent, param=ParamSTL()):
+
+        super(SimTimeLess, self).__init__(thePuzzle)
+
+        # self.init_board = init_board  # Initial board
+        # self.sol_board = sol_board  # Solution board
+        # self.cur_board = init_board  # The current board. At first it will be only the inital board
         self.agent = agent  # The agent
         self.param = param
 
-        # let the agent be aware of the solution board
-        self.agent.setSolBoard(sol_board)
-
-    def simulate(self, vis=False, vis_pause_time=1, **kwargs):
-        """
-        Simulate until done
-
-        @param[in]  vis             If True, will visualize the scene after each simulation step
-        @param[in]  vis_pause_time  Pause time for visualization
-        @param[in]  **kwargs        The parameter for visualize function. See its API for detail
-        """
-        SUCCESS = True
-        while (SUCCESS):
-            SUCCESS = self.simulate_step()
-            if vis:
-                self.display(mode="scene", **kwargs)
-                plt.pause(0.01)
-
-    def simulate_step(self):
-        """
-        The simulation step.
-        For the timeless simulator, the simulation step will fully execute the agent's next action
-        """
-        theSuccessFlag, action, action_arg = self.agent.process(self.cur_board, execute=True)
-        return theSuccessFlag
-
-    def display(self, mode="scene", pickColorA=None, title=None, ax=None):
-        """
-        Visualization. It offers the following functionality
-        1. Visualize the initial board
-        2. Visualize the solution board
-        3. Visualize the current scene, including the agent and the current board
-
-        @param[in]  mode            "initBoard", "solBoard", "scene"(default)
-        @param[in]  pickColorA      The option to display a different color of the agent when 
-                                    the agent has a puzzle piece in hand.
-                                    It is only for visualization, the real color of the agent won't be changed
-                                    If None(default), then will use the original agent color for visualization
-        @param[in]  title           The title
-        @param[in]  ax              The axis for drawing
-        """
-        # determine the axis
-        if ax is None:
-            plt.figure()
-            ax = plt.gca()
-
-        # visualization 
-        canvas = np.ones(
+        self.canvas = np.zeros(
             (self.param.canvas_H, self.param.canvas_W, 3),
             dtype=np.uint8
-        ) * 255
+        )
 
-        if mode == "initBoard":
-            self.init_board.toImage(canvas, BOUNDING_BOX=False)
+        # cached action, argument, and time
+        self.cache_action = None  # The next action to execute
+        self.cache_arg = None  # The next action argument
 
-        elif mode == "solBoard":
-            self.sol_board.toImage(canvas, BOUNDING_BOX=False)
+        # let the agent be aware of the solution board
+        # self.agent.setSolBoard(sol_board)
 
-        elif mode == "scene":
-            # the current board
-            self.cur_board.toImage(canvas, BOUNDING_BOX=False)
+    def display(self, ID_DISPLAY=True, CONTOUR_DISPLAY=True):
+        """
+        @brief  Displays the current puzzle board.
 
-            # the agent
-            if (pickColorA is not None) and (self.agent.cache_piece is not None):
-                appear_cache = deepcopy(self.agent.app.y.appear)
-                new_appear = np.repeat(pickColorA[np.newaxis, :], repeats=self.agent.app.y.appear.shape[0], axis=0)
-                self.agent.app.y.appear = new_appear
-                self.agent.placeInImage(canvas, CONTOUR_DISPLAY=False)
-                self.agent.app.y.appear = appear_cache
-            else:
-                self.agent.placeInImage(canvas, CONTOUR_DISPLAY=False)
+        Args:
+            ID_DISPLAY: Flag indicating ID_DISPLAY or not.
+            CONTOUR_DISPLAY: Flag indicating CONTOUR_DISPLAY or not.
+        """
 
-        ax.imshow(canvas)
-        ax.set_title(title)
+        if not self.fig:
+            self.fig = plt.figure()
+
+        theImage = self.puzzle.toImage(np.zeros_like(self.canvas), ID_DISPLAY=ID_DISPLAY, BOUNDING_BOX=False)
+        self.agent.placeInImage(theImage, CONTOUR_DISPLAY=CONTOUR_DISPLAY)
+        im = plt.imshow(theImage)
+
+        def press(event):
+            print('press', event.key)
+            sys.stdout.flush()
+
+            if event.key == 'up':
+                self.cache_action = "move"
+                self.cache_arg = np.array([0, -self.param.displacement]) + self.agent.app.rLoc
+            elif event.key == 'left':
+                self.cache_action = "move"
+                self.cache_arg = np.array([-self.param.displacement, 0]) + self.agent.app.rLoc
+            elif event.key == 'down':
+                self.cache_action = "move"
+                self.cache_arg = np.array([0, self.param.displacement]) + self.agent.app.rLoc
+            elif event.key == 'right':
+                self.cache_action = "move"
+                self.cache_arg = np.array([self.param.displacement, 0]) + self.agent.app.rLoc
+            elif event.key == 'z':
+                self.cache_action = "pick"
+                self.cache_arg = None
+            elif event.key == 'c':
+                self.cache_action = "place"
+                self.cache_arg = None
+
+            self.agent.execute(self.puzzle, self.cache_action, self.cache_arg)
+
+            cache_image = self.puzzle.toImage(np.zeros_like(self.canvas), ID_DISPLAY=ID_DISPLAY,
+                                              BOUNDING_BOX=False)
+
+            theImage = deepcopy(cache_image)
+            self.agent.placeInImage(theImage, CONTOUR_DISPLAY=CONTOUR_DISPLAY)
+            plt.pause(0.001)
+
+            im.set_data(theImage)
+            self.fig.canvas.draw()
+
+        self.fig.canvas.mpl_connect('key_press_event', press)
+
+        plt.show()
