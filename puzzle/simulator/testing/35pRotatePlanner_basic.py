03222876957bdaa8595d7dce8d01d74529ff2560
yunzhi <yunzhi.lin@gatech.edu>
2022-01-26 14:42:17 +0100

    update test script for more cases

--- puzzle/simulator/testing/35pRotatePlanner_basic.py
+++ puzzle/simulator/testing/35pRotatePlanner_basic.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 # ============================ 35pRotatePlanner_basic ===========================
 #
-# @brief    Test script with command from the solver. (35p img)
+# @brief    Test script with command from the planner. Test for success rate. (35p img)
 #
 # ============================ 35pRotatePlanner_basic ===========================
 
@@ -13,11 +13,12 @@
 #
 # ============================ 35pRotatePlanner_basic ===========================
 
-# Todo: You can change the flag here
+# Change the Settings here
 
 ROTATION_ENABLED = True
 # ROTATION_ENABLED = False
 
+NUM = 50
 
 # ==[0] Prep environment
 import glob
@@ -38,96 +39,112 @@ from puzzle.simulator.planner import Planner
 fpath = os.path.realpath(__file__)
 cpath = fpath.rsplit('/', 1)[0]
 
-np.random.seed(100)
-# ==[1] Read the source image and template.
-#
-theImageSol = cv2.imread(cpath + '/../../testing/data/balloon.png')
-# theImageSol = cv2.imread(cpath + '/../../testing/data/church.jpg')
-# theImageSol = cv2.imread(cpath + '/../../testing/data/cocacola.jpg')
-# theImageSol = cv2.imread(cpath + '/../../testing/data/map.jpg')
+# Debug only
+# np.random.seed(100)
 
-theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_35p.png')
-# theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_15p_123rf.png')
+Image_list = ['balloon.png', 'church.jpg', 'cocacola.jpg', 'duck.jpg', 'map.jpg']
+Res = np.zeros(len(Image_list))
 
-theGridMea, theGridSol, gt_pAssignments = create_synthetic_puzzle(theImageSol=theImageSol, theMaskSol_src=theMaskSol_src,
-                                                 ROTATION_ENABLED=ROTATION_ENABLED,verbose=False)
+for trial_index in range(NUM):
 
-# ==[3] Create a manager & simple solver and integrate them into a planner
-#
+    print(f'Trial {trial_index}:')
 
-theManager = Manager(theGridSol, ManagerParms(matcher=Sift()))
-theSolver = Simple(theGridSol, theGridMea)
-thePlanner = Planner(theSolver, theManager, ParamGrid(areaThresholdLower=1000))
+    # ==[1] Read the source image and template.
+    #
+    # theImageSol = cv2.imread(cpath + '/../../testing/data/' + Image_list[np.random.randint(0,len(Image_list))])
 
-# ==[4] Create a simulator for display
-#
-theSim = Basic(theGridMea, thePlanner=thePlanner, theParams=ParamBasic(3000,6000))
+    # NUM/len(Image_list) for each image
+    theImageSol = cv2.imread(cpath + '/../../testing/data/' + Image_list[trial_index%len(Image_list)])
 
-# ==[5] Start the solver to take turns, execute the plan, and display the updated board.
-#
+    theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_35p.png')
+    # theMaskSol_src = cv2.imread(cpath + '/../../testing/data/puzzle_15p_123rf.png')
 
-plt.ion()
+    theGridMea, theGridSol, gt_pAssignments = create_synthetic_puzzle(theImageSol=theImageSol, theMaskSol_src=theMaskSol_src,
+                                                     ROTATION_ENABLED=ROTATION_ENABLED,verbose=False)
 
-# saveMe = True
-saveMe = False
+    # ==[2] Create a manager & simple solver and integrate them into a planner
+    #
 
-if saveMe:
-    filename_list = glob.glob(cpath + f'/data/35pRotatePlanner_step*.png')
-    for filename in filename_list:
-        os.remove(filename)
+    theManager = Manager(theGridSol, ManagerParms(matcher=Sift()))
+    theSolver = Simple(theGridSol, theGridMea)
+    thePlanner = Planner(theSolver, theManager, ParamGrid(areaThresholdLower=1000))
 
-finishFlag = False
-i = 0
+    # ==[3] Create a simulator for display
+    #
+    theSim = Basic(theGridMea, thePlanner=thePlanner, theParams=ParamBasic(3000,6000))
 
-while 1:
+    # ==[4] Start the solver to take turns, execute the plan, and display the updated board.
+    #
 
-    # Since we use the same instance in the simulator and the solver,
-    # it will update automatically.
-    theSim.display(ID_DISPLAY=True, BOUNDING_BOX=False)
+    plt.ion()
 
-    theSim.fig.suptitle(f'Step {i}', fontsize=20)
-    plt.pause(1)
-
-    if finishFlag:
-        break
-    if i == 0:
-        # Display the original one at the very beginning
-        print(f'The original measured board')
+    # saveMe = True
+    saveMe = False
 
     if saveMe:
-        theSim.fig.savefig(cpath + f'/data/35pRotatePlanner_step{str(i).zfill(3)}.png')
-
-    print(f'Step {i + 1}:')
-
-    # One Step
-    plan = thePlanner.process(theSim.puzzle, COMPLETE_PLAN=True, SAVED_PLAN=False)
-
-    # Step by step
-    # plan = thePlanner.process(theSim.puzzle, COMPLETE_PLAN=False)
-
-    finishFlag = theSim.takeAction(plan)
-
-    i = i + 1
-
-# # Only make sense when all the pieces have been extracted
-# gt_pAssignments
-# thePlanner.manager.pAssignments
-
-# Call the progress to compare rLoc
-print('Progress:', theSim.progress(gt_pAssignments))
-
-plt.ioff()
-plt.show()
-# plt.draw()
-
-if saveMe:
-    # Build GIF
-    with imageio.get_writer(cpath + f'/data/35pRotatePlanner.gif', mode='I', fps=1) as writer:
         filename_list = glob.glob(cpath + f'/data/35pRotatePlanner_step*.png')
-        filename_list.sort()
         for filename in filename_list:
-            image = imageio.imread(filename)
-            writer.append_data(image)
+            os.remove(filename)
+
+    finishFlag = False
+    i = 0
+
+    while 1:
+
+        # Since we use the same instance in the simulator and the solver,
+        # it will update automatically.
+        theSim.display(ID_DISPLAY=True, BOUNDING_BOX=False)
+
+        theSim.fig.suptitle(f'Step {i}', fontsize=20)
+        plt.pause(1)
+
+        if finishFlag:
+            break
+        if i == 0:
+            # Display the original one at the very beginning
+            print(f'The original measured board')
+
+        if saveMe:
+            theSim.fig.savefig(cpath + f'/data/35pRotatePlanner_step{str(i).zfill(3)}.png')
+
+        print(f'Step {i + 1}:')
+
+        # One Step
+        plan = thePlanner.process(theSim.puzzle, COMPLETE_PLAN=True, SAVED_PLAN=False)
+
+        # Step by step
+        # plan = thePlanner.process(theSim.puzzle, COMPLETE_PLAN=False)
+
+        finishFlag = theSim.takeAction(plan)
+
+        i = i + 1
+
+    # Call the progress to compare rLoc
+    thePercent = theSim.progress(gt_pAssignments)
+    print('Progress:', thePercent)
+
+    Res[trial_index%len(Image_list)]+=float(thePercent.split('%')[0])/100.0
+
+    plt.ioff()
+    # plt.show()
+    # plt.draw()
+    plt.pause(1)
+    plt.close()
+
+    if saveMe:
+        # Build GIF
+        with imageio.get_writer(cpath + f'/data/35pRotatePlanner.gif', mode='I', fps=1) as writer:
+            filename_list = glob.glob(cpath + f'/data/35pRotatePlanner_step*.png')
+            filename_list.sort()
+            for filename in filename_list:
+                image = imageio.imread(filename)
+                writer.append_data(image)
+
+# ==[5] Print final result
+Res = Res/(NUM/len(Image_list))
+print('\nAverage result:\n')
+for i in range(len(Image_list)):
+    print(f'Average success rate for Image {i}:', '{:.1%}'.format(Res[i]))
 
 #
 # ============================ 35pRotatePlanner_basic ===========================
