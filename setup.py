3ba8126fdb300dc39281e8794c311e4a6ab6ba05
Anthony Miyaguchi <acmiyaguchi@gmail.com>
2023-02-17 15:15:52 -0500

    Use find_package

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='puzzle',
-      version='1.0',
-      description='Classes implementing detection based puzzle solving pipelines.',
-      url="https://github.com/ivapylibs/puzzle_solver",
-      author='IVALab',
-      packages=['puzzle'],
-      install_requires=['numpy', 'scipy>=1.8.1', 'matplotlib', 'opencv-contrib-python', 
-        'scikit-image', 'scikit-learn','similaritymeasures', 'pygame']
-      )
+setup(
+    name="puzzle",
+    version="1.0.1",
+    description="Classes implementing detection based puzzle solving pipelines.",
+    url="https://github.com/ivapylibs/puzzle_solver",
+    author="IVALab",
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy>=1.8.1",
+        "matplotlib",
+        "opencv-contrib-python",
+        "scikit-image",
+        "scikit-learn",
+        "similaritymeasures",
+        "pygame",
+    ],
+)
