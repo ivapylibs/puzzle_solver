2d31ffff50e86c523a9ef22b3a26c2e07437670e
Patricio A. Vela <pvela@gatech.edu>
2024-10-05 13:58:44 -0400

    readying to pull

--- puzzle/clusters/byColor.py
+++ puzzle/clusters/byColor.py
@@ -1,8 +1,8 @@
-# ================================ puzzle.clusters.byColor ================================
+#============================= puzzle.clusters.byColor =============================
 #
 # @brief    Extract color features for all the pieces in a given puzzle board.
 #
-# ================================ puzzle.clusters.byColor ================================
+#============================= puzzle.clusters.byColor =============================
 #
 # @file     byColor.py
 #
@@ -10,11 +10,11 @@
 # @date     2021/08/29 [created]
 #
 #
-# ================================ puzzle.clusters.byColor ================================
+#============================= puzzle.clusters.byColor =============================
 
 from dataclasses import dataclass
 
-# ===== Environment / Dependencies
+#===== Environment / Dependencies
 #
 import numpy as np
 import cv2
@@ -25,11 +25,15 @@ from scipy.optimize import linear_sum_assignment
 from puzzle.builder.board import Board
 from puzzle.piece.histogram import Histogram
 
-# ===== Helper Elements
+#===== Helper Elements
 #
 
 @dataclass
 class ParamColorCluster:
+    '''!
+    Configuration parameter struct for byColor clustering.
+    '''
+    # @todo Should convert to a AlgConfig instance.
     tauDist: float = 0.5
     cluster_num: int = 2
     cluster_mode: str = 'threshold' # 'threshold' or 'number'
@@ -38,23 +42,30 @@ class ParamColorCluster:
 # ================================ puzzle.clusters.byColor ================================
 #
 class ByColor(Board):
+    '''!
+    @brief  A puzzle piece clustering method based on color.  The feature extractor
+            should be based on color.
+    '''
 
+
+    #========================== ByColor __init__ =========================
+    #
     def __init__(self, thePuzzle, extractor=Histogram(), theParams=ParamColorCluster):
-        """
+        '''!
         @brief  Constructor for the byColor class.
 
         Args:
             thePuzzle: The input puzzle board.
             extractor: A matcher instance.
             theParams: The param for threshold.
-        """
+        '''
 
         super(ByColor, self).__init__(thePuzzle)
 
         self.feaExtractor = extractor
 
         # A list of feature for all the puzzle pieces
-        self.feature = []
+        self.feature  = []
         self.feaLabel = []
 
         self.feature_dict = {}
@@ -64,29 +75,42 @@ class ByColor(Board):
 
         self.params = theParams
 
+    #============================== process ==============================
+    #
     def process(self):
-        """
+        '''!
         @brief Extract color features from the data.
-        """
 
+        Since this instance is a board, the presumption is that there is a measurement
+        available in the stored board data.  If there is nothing then there can be no
+        clustering achieved.
+        '''
+
+        # For each piece, collect its feature signature (based on color!!).
         for key in self.pieces:
             piece = self.pieces[key]
             self.feature.append(self.feaExtractor.colorFeaExtract(piece).flatten())
-        self.feature = np.array(self.feature)
+
+        self.feature = np.array(self.feature)       # Convert to matrix for distance scoring.
 
         distance_matrix = np.zeros((len(self.feature), len(self.feature)))
 
+        # @todo This code needs to be overloaded with the proper match code that
+        #       already calls this.
         for i in range(len(self.feature)):
             for j in range(len(self.feature)):
                 # https://docs.opencv.org/5.x/d8/dc8/tutorial_histogram_comparison.html
                 # https://vovkos.github.io/doxyrest-showcase/opencv/sphinx_rtd_theme/enum_cv_HistCompMethods.html
                 distance_matrix[i][j] = cv2.compareHist(self.feature[i], self.feature[j], 3)
 
-        if self.params.cluster_mode == 'threshold':
-            # Using threshold
-            model = AgglomerativeClustering(affinity='precomputed', n_clusters=None, linkage='complete', distance_threshold=self.params.tauDist).fit(distance_matrix)
-        elif self.params.cluster_mode == 'number':
-            model = AgglomerativeClustering(affinity='precomputed', n_clusters=self.params.cluster_num, linkage='complete').fit(distance_matrix)
+        if self.params.cluster_mode == 'threshold':     # Using threshold clustering
+            model = AgglomerativeClustering(affinity='precomputed', n_clusters=None, \
+               linkage='complete', distance_threshold=self.params.tauDist).fit(distance_matrix)
+
+        elif self.params.cluster_mode == 'number':      # Targeting a cluster quantity.
+            model = AgglomerativeClustering(affinity='precomputed', \
+                n_clusters=self.params.cluster_num, linkage='complete').fit(distance_matrix)
+
         else:
             raise ValueError('Unknown cluster mode!')
 
@@ -110,8 +134,10 @@ class ByColor(Board):
 
         # print(model.labels_)
 
+    #============================== score ==============================
+    #
     def score(self, cluster_id_pred_dict, method='label'):
-        """
+        '''!
         @brief  Score the clustering result. See https://scikit-learn.org/stable/modules/clustering.html#overview-of-clustering-methods
 
         Args:
@@ -120,7 +146,7 @@ class ByColor(Board):
 
         Returns:
             The score of the clustering result.
-        """
+        '''
 
         # Does not matter if the id has the exact same value as the true label or not
 
@@ -171,8 +197,5 @@ class ByColor(Board):
             return score
 
 
-
-
-
 #
-# ================================ puzzle.clusters.byColor ================================
+#============================= puzzle.clusters.byColor =============================
